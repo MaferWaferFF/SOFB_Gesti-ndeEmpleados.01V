8e24f04c68c4737aa30caefa2eb4b3ae2cf3d5ed
Grandez <maferwaferFF@hotmail.com>
2025-05-03 18:31:29 -0500

    Initial commit(primer avance del proyecto)

---
+++ Vistas/plantilla.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Sistema Empleados</title>        
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link rel="stylesheet" href="css/Style.css">
+    </head>
+    <body>
+        <!--NAVBAR-->
+        <header>
+            <div class="left">
+                <div class="menu-container" id="menuContainer">
+                    <div class="menu" id="menu">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <div class="brand">
+                    <img src="img/logo.svg" alt="icon.logo" class="logo">
+                    <span class="name">PINKI</span>
+                </div>
+            </div>
+
+            <div class="rigth">
+                <a href=#>
+                    <img src="img/soporteManual.svg" alt="soporte">
+                </a>
+                <div class="user-container">
+                    <img src="img/alienUser.svg" alt="img-user" class="user">
+                    <span class="nombre-usuario">Nombre Usuario</span>
+                </div>                
+            </div>
+        </header>
+        <!--NAVBAR-->
+        <!--SIDERBAR-->
+        <div class="sidebar" id="sidebar">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#" class="selected">
+                            <img src="img/panel.svg" alt="">
+                            <span>Panel</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/empleado.svg" alt="">
+                            <span>Empleados</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/contrato.svg" alt="">
+                            <span>Contrato</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/pagos.svg" alt="">
+                            <span>Pagos</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/reportes.svg" alt="">
+                            <span>Reportes</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+         <!--SIDERBAR-->
+        <main class="main" id="main">
+            <p>no puedooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</p>
+        </main>       
+       <script src="js/Script.js"></script>
+    </body>
+</html>
+
