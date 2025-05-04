09b60e0bcacd8087b06c841f5a05ae50bc4dd3cd
Grandez <maferwaferFF@hotmail.com>
2025-05-04 10:52:06 -0500

    Modificación empleadoVista

---
+++ Vistas/Contenido/Empleado/rmpleadoVista-modificar.php
@@ -0,0 +1,8 @@
+<?php 
+    include('../../Modulos/Cabecera.php'); 
+    include('../../Modulos/Sidebar.php');
+    include('../../Modulos/NavLateral.php');
+    ?>
+
+    
+<?php include('../../Modulos/Piecera.php'); ?>
